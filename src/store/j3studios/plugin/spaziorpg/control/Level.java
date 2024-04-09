90e60ab9ff53b259e5f5c5434509ffefc4a63c3f
josne <josne@DESKTOP-7DQ2NEU>
2024-04-09 01:18:53 -0400

    Actualización; Se programó una versión nueva SQL para testear y se implementó el sistema de niveles.

---
+++ src/store/j3studios/plugin/spaziorpg/control/Level.java
@@ -0,0 +1,52 @@
+package store.j3studios.plugin.spaziorpg.control;
+
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import store.j3studios.plugin.spaziorpg.database.SQL;
+import store.j3studios.plugin.spaziorpg.utils.Tools;
+
+public class Level {
+
+    private static Level ins;
+    public static Level get() {
+        if (ins == null) {
+            ins = new Level();
+        }
+        return ins;
+    }
+    
+    //
+    
+    public Integer getNeededExp (Player player) {
+        Integer playerLevel = (Integer) SQL.get().getPlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_LEVEL);
+        return playerLevel * 750;
+    }
+    
+    public void giveExp (Player player, Integer experience) {
+        Integer playerLevel = (Integer) SQL.get().getPlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_LEVEL);
+        Integer playerExp = (Integer) SQL.get().getPlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_EXP);
+        Integer playerTotalExp = (Integer) SQL.get().getPlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_TOTAL_EXP);
+        Integer maxExp = playerLevel * 750;        
+        if ((playerExp+experience) < maxExp) {
+            SQL.get().updatePlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_EXP, playerExp+experience);
+            SQL.get().updatePlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_TOTAL_EXP, playerTotalExp+experience);
+        } else {
+            Integer sobrante = (playerExp+experience)-maxExp;
+            Integer giving = experience-sobrante;
+            SQL.get().updatePlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_EXP, playerExp+giving);
+            SQL.get().updatePlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_TOTAL_EXP, playerTotalExp+giving);
+            checkLevelup(player);
+            giveExp(player,sobrante);
+        }
+    }
+    
+    public void checkLevelup (Player player) {
+        Integer playerLevel = (Integer) SQL.get().getPlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_LEVEL);
+        if (playerLevel == 60)
+            return;
+        SQL.get().updatePlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_LEVEL, playerLevel+1);
+        SQL.get().updatePlayer(player, SQL.DataType.PLAYER_STATS, SQL.UpdateType.PLAYER_STATS_EXP, 0);
+        Tools.get().playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 5.0f, 1.0f);
+    }
+    
+}
--- src/store/j3studios/plugin/spaziorpg/database/SQL.java
+++ src/store/j3studios/plugin/spaziorpg/database/SQL.java
@@ -3,8 +3,10 @@ package store.j3studios.plugin.spaziorpg.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.bukkit.entity.Player;
 import store.j3studios.plugin.spaziorpg.RPG;
 
 public class SQL {
@@ -33,10 +35,10 @@ public class SQL {
         PLAYER_STATS ("PLAYER_ACCOUNT"),
         PLAYER_SKILLS ("PLAYER_SKILLS"),
         PLAYER_JOBS ("PLAYER_JOBS"),
+        PLAYER_BANK ("PLAYER_BANK"),
         
         CLAN_STATS ("CLAN_STATS"),
                 
-        BANK_ACCOUNTS ("BANK_ACCOUNTS"),
         BANK_BONOS ("BANK_BONOS"),
         BANK_STATS ("BANK_STATS");
         
@@ -96,8 +98,8 @@ public class SQL {
                 case PLAYER_STATS -> st.executeUpdate("CREATE TABLE IF NOT EXISTS `" + dataType + "` (`uuid` VARCHAR(255), `username` VARCHAR(255), `level` INT NOT NULL DEFAULT '1', `exp` INT NOT NULL DEFAULT '0', `totalExp` INT NOT NULL DEFAULT '0')");
                 case PLAYER_SKILLS -> st.executeUpdate("CREATE TABLE IF NOT EXISTS `" + dataType + "` (`uuid` VARCHAR(255), `username` VARCHAR(255), `strength` INT NOT NULL DEFAULT '0', `agility` INT NOT NULL DEFAULT '0', `life` INT NOT NULL DEFAULT '0', `defense` INT NOT NULL DEFAULT '0')");
                 case PLAYER_JOBS -> st.executeUpdate("CREATE TABLE IF NOT EXISTS `" + dataType + "` (`uuid` VARCHAR(255), `username` VARCHAR(255), `job_1` VARCHAR(255), `job_2` VARCHAR(255), `jobs_level` LONGTEXT, `jobs_exp` LONGTEXT, `jobs_totalExp` LONGTEXT)");
+                case PLAYER_BANK -> st.executeUpdate("CREATE TABLE IF NOT EXISTS `" + dataType + "` (`uuid` VARCHAR(255), `username` VARCHAR(255), `account` VARCHAR(255), `level` INT NOT NULL DEFAULT '1', `balance` DOUBLE NOT NULL DEFAULT '0.0', `transactions` LONGTEXT, `claimedBonos` LONGTEXT)");
                 //
-                case BANK_ACCOUNTS -> st.executeUpdate("CREATE TABLE IF NOT EXISTS `" + dataType + "` (`uuid` VARCHAR(255), `username` VARCHAR(255), `account` VARCHAR(255), `level` INT NOT NULL DEFAULT '1', `balance` DOUBLE NOT NULL DEFAULT '0.0', `transactions` LONGTEXT, `claimedBonos` LONGTEXT)");
                 case BANK_BONOS -> st.executeUpdate("CREATE TABLE IF NOT EXISTS `" + dataType + "` (`id` INT NOT NULL PRIMARY KEY, `name` VARCHAR(255), `stock` INT, `level` INT, `value` DOUBLE)");
                 case BANK_STATS -> st.executeUpdate("CREATE TABLE IF NOT EXISTS `" + dataType + "` (`accounts` INT NOT NULL DEFAULT '0', `totalMoney` DOUBLE NOT NULL DEFAULT '0.0', `partners` LONGTEXT)");
             }
@@ -110,6 +112,139 @@ public class SQL {
                 e.printStackTrace();
             }
         }
+    }
+    
+    /*
+        UPDATE / GET : PLAYER STATS
+    */
+    
+    public enum UpdateType {
+        PLAYER_STATS_USERNAME ("username"),
+        PLAYER_STATS_LEVEL ("level"),
+        PLAYER_STATS_EXP ("exp"),
+        PLAYER_STATS_TOTAL_EXP ("totalExp"),
+        
+        PLAYER_SKILLS ("PLAYER_SKILLS"),
+        PLAYER_JOBS ("PLAYER_JOBS"),
+        
+        CLAN_STATS ("CLAN_STATS"),
+                
+        BANK_ACCOUNTS ("BANK_ACCOUNTS"),
+        BANK_BONOS ("BANK_BONOS"),
+        BANK_STATS ("BANK_STATS");
+        
+        private final String dataType;
+        private UpdateType (String str) { this.dataType = str; }
+        public String getUpdateType() {
+            return this.dataType;
+        }
+    }
+    
+    /*
+        CREATE / UPDATE / GET - PLAYER STATS
+    */
+    
+    public void createPlayer (Player player, DataType dataType) {
+        try {
+            PreparedStatement st1 = SQL.get().getConnection().prepareStatement("SELECT * FROM `" + dataType.getDataType() + "` WHERE `uuid` = ? LIMIT 1;");
+            st1.setString(1, player.getUniqueId().toString());
+            st1.executeQuery();
+            ResultSet rs = st1.getResultSet();
+            if (!rs.next()) {
+                switch (dataType) {
+                    case PLAYER_STATS ->                         {
+                            PreparedStatement st2 = SQL.get().getConnection().prepareStatement("INSERT INTO `" + dataType.getDataType() + "` VALUES (?, ?, ?, ?, ?)");
+                            st2.setString(1, player.getUniqueId().toString());
+                            st2.setString(2, player.getName());
+                            st2.setInt(3, 1);
+                            st2.setInt(4, 0);
+                            st2.setInt(5, 0);
+                            st2.executeUpdate();
+                            st2.close();
+                        }
+                    case PLAYER_SKILLS ->                         {
+                            PreparedStatement st2 = SQL.get().getConnection().prepareStatement("INSERT INTO `" + dataType.getDataType() + "` VALUES (?, ?, ?, ?, ?, ?)");
+                            st2.setString(1, player.getUniqueId().toString());
+                            st2.setString(2, player.getName());
+                            st2.setInt(3, 0);
+                            st2.setInt(4, 0);
+                            st2.setInt(5, 0);
+                            st2.setInt(6, 0);
+                            st2.executeUpdate();
+                            st2.close();
+                        }
+                    case PLAYER_JOBS ->                         {
+                            PreparedStatement st2 = SQL.get().getConnection().prepareStatement("INSERT INTO `" + dataType.getDataType() + "` VALUES (?, ?, ?, ?, ?, ?, ?)");
+                            st2.setString(1, player.getUniqueId().toString());
+                            st2.setString(2, player.getName());
+                            st2.setString(3, "");
+                            st2.setString(4, "");
+                            st2.setString(5, "");
+                            st2.setString(6, "");
+                            st2.setString(7, "");
+                            st2.executeUpdate();
+                            st2.close();
+                        }
+                    case PLAYER_BANK ->                         { 
+                            PreparedStatement st2 = SQL.get().getConnection().prepareStatement("INSERT INTO `" + dataType.getDataType() + "` VALUES (?, ?, ?, ?, ?, ?, ?)");
+                            st2.setString(1, player.getUniqueId().toString());
+                            st2.setString(2, player.getName());
+                            st2.setString(3, "");
+                            st2.setInt(4, 1);
+                            st2.setDouble(5, 0.0);
+                            st2.setString(6, "");
+                            st2.setString(7, "");
+                            st2.executeUpdate();
+                            st2.close();
+                        }
+                    default -> {
+                    }
+                }
+            }
+            rs.close();
+            st1.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void updatePlayer (Player player, DataType dataType, UpdateType updateType, Object value) {
+        RPG.get().getServer().getScheduler().runTaskAsynchronously(RPG.get(), () -> {
+            try {
+                PreparedStatement st = SQL.get().getConnection().prepareStatement("SELECT * FROM `" + dataType.getDataType() + "` WHERE `uuid` = ? LIMIT 1;");
+                st.setString(1, player.getUniqueId().toString());
+                st.executeQuery();
+                ResultSet rs = st.getResultSet();
+                if (!rs.next()) {
+                    createPlayer(player, dataType);
+                } else {
+                    PreparedStatement st2 = SQL.get().getConnection().prepareStatement("UPDATE `" + dataType.getDataType() + "` SET `" + updateType.getUpdateType() + "` = ? WHERE `ùuid` = ? LIMIT 1;");
+                    st2.setObject(1, value);
+                    st2.setString(2, player.getUniqueId().toString());
+                    st2.executeUpdate();
+                    st2.close();
+                }
+                rs.close();
+                st.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });  
+    }
+    
+    public Object getPlayer (Player player, DataType dataType, UpdateType updateType) {
+        Object value = null;
+        try {
+            assert (SQL.get().getConnection() != null);
+            PreparedStatement st = SQL.get().getConnection().prepareStatement("");
+            st.setString(1, player.getUniqueId().toString());
+            st.executeQuery();
+            ResultSet rs = st.getResultSet();
+            value = rs.next() ? rs.getObject(updateType.getUpdateType()) : null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
